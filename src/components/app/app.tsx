972ca89ae50206785d860cf9b6ab63dedd3c71fa
g.kiselev <g.kiselev@skbkontur.ru>
2025-10-04 21:01:29 +0500

    add: private-route.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,11 @@
 ﻿import MainPage from '../../pages/main-page/main-page.tsx';
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
-import {AppRoute} from "../../const.ts";
+import {AppRoute, AuthorizationStatus} from "../../const.ts";
 import LoginPage from "../../pages/login-page/login-page.tsx";
 import FavoritesPage from "../../pages/favorites-page/favorites-page.tsx";
 import OfferPage from "../../pages/offer-page/offer-page.tsx";
 import NotFoundPage from "../../pages/not-found-page/not-found-page.tsx";
+import PrivateRoute from "../private-route/private-route.tsx";
 
 type AppProps = {
   placesCount: number;
@@ -24,7 +25,11 @@ function App({placesCount}: AppProps): JSX.Element {
         />
         <Route
           path={AppRoute.Favorites}
-          element={<FavoritesPage/>}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <FavoritesPage/>
+            </PrivateRoute>
+          }
         />
         <Route
           path={AppRoute.Offers}
--- src/const.ts
+++ src/const.ts
@@ -8,3 +8,9 @@ export enum AppRoute {
   Favorites = '/favorites',
   Offers = '/offer/:id'
 }
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN'
+}
