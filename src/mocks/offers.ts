aafd06de538e96ddc77b16d86ef74a9917491aa7
g.kiselev <g.kiselev@skbkontur.ru>
2025-10-16 00:12:15 +0500

    finish

--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -6,7 +6,7 @@ export const offers: Offers = [
     title: 'Beautiful & luxurious studio at great location',
     placeType: 'Apartment',
     location: 'Amsterdam',
-    isPremium: false,
+    isPremium: true,
     isFavorite: false,
     rating: 4.8,
     price: 120,
@@ -38,5 +38,119 @@ export const offers: Offers = [
       avatarSrc: 'img/avatar-angelina.jpg',
       isPro: true
     }
-  }
+  },
+  {
+    id: '82dae9d9-ed35-44e5-ab1d-d88f667d841a',
+    title: 'Wood and stone place',
+    placeType: 'Apartment',
+    location: 'Amsterdam',
+    isPremium: false,
+    isFavorite: false,
+    rating: 4,
+    price: 80,
+    bedrooms: 1,
+    maxAdults: 2,
+    previewImage: {
+      'src': 'img/room.jpg'
+    },
+    images: [
+      {
+        'src': 'img/room.jpg'
+      },
+      {
+        'src': 'img/apartment-01.jpg'
+      },
+      {
+        'src': 'img/apartment-02.jpg'
+      },
+      {
+        'src': 'img/apartment-03.jpg'
+      },
+      {
+        'src': 'img/studio-01.jpg'
+      }
+    ],
+    insideItems: ['Wi-Fi', 'Washing machine', 'Towels', 'Heating', 'Coffee machine', 'Baby seat', 'Kitchen', 'Dishwasher', 'Cable TV', 'Fridge'],
+    host: {
+      name: 'Angelina',
+      avatarSrc: 'img/avatar-angelina.jpg',
+      isPro: true
+    }
+  },
+  {
+    id: 'ac183d04-fd29-4e21-9d79-f271497d0369',
+    title: 'Nice, cozy, warm big bed apartment',
+    placeType: 'Room',
+    location: 'Amsterdam',
+    isPremium: false,
+    isFavorite: true,
+    rating: 3.5,
+    price: 150,
+    bedrooms: 3,
+    maxAdults: 4,
+    previewImage: {
+      'src': 'img/apartment-03.jpg'
+    },
+    images: [
+      {
+        'src': 'img/room.jpg'
+      },
+      {
+        'src': 'img/apartment-01.jpg'
+      },
+      {
+        'src': 'img/apartment-02.jpg'
+      },
+      {
+        'src': 'img/apartment-03.jpg'
+      },
+      {
+        'src': 'img/studio-01.jpg'
+      }
+    ],
+    insideItems: ['Wi-Fi', 'Washing machine', 'Towels', 'Heating', 'Coffee machine', 'Baby seat', 'Kitchen', 'Dishwasher', 'Cable TV', 'Fridge'],
+    host: {
+      name: 'Angelina',
+      avatarSrc: 'img/avatar-angelina.jpg',
+      isPro: false
+    }
+  },
+  {
+    id: '2df10e4b-4743-47e7-9739-38d30c3b6158',
+    title: 'Nice!',
+    placeType: 'Room',
+    location: 'Amsterdam',
+    isPremium: false,
+    isFavorite: false,
+    rating: 4.2,
+    price: 30,
+    bedrooms: 3,
+    maxAdults: 4,
+    previewImage: {
+      'src': 'img/apartment-01.jpg'
+    },
+    images: [
+      {
+        'src': 'img/room.jpg'
+      },
+      {
+        'src': 'img/apartment-01.jpg'
+      },
+      {
+        'src': 'img/apartment-02.jpg'
+      },
+      {
+        'src': 'img/apartment-03.jpg'
+      },
+      {
+        'src': 'img/studio-01.jpg'
+      }
+    ],
+    insideItems: ['Wi-Fi', 'Washing machine', 'Towels', 'Heating', 'Coffee machine', 'Baby seat', 'Kitchen', 'Dishwasher', 'Cable TV', 'Fridge'],
+    host: {
+      name: 'Angelina',
+      avatarSrc: 'img/avatar-angelina.jpg',
+      isPro: false
+    }
+  },
 ];
